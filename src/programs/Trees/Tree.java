442a3c199bed77604d885478bf990bfc3ea0f729
Julian Saldana <61172337+julianesaldana@users.noreply.github.com>
2020-07-31 10:51:12 -0700

    Add files via upload

--- src/programs/Trees/Tree.java
+++ src/programs/Trees/Tree.java
@@ -1,35 +1,151 @@
 package programs.Trees;
 
-import programs.LinkedList.Node;
-
 public class Tree {
-    private treeNode head;      // list is going to use a head and utilize "next" of nodes
-    public int size = 0;
+    // setting up tree nodes
+    public static class treeNode {
+        private int value;
+        private treeNode lowerChild;  // recursively defined object   // recursion
+        private treeNode upperChild;
 
-    public Tree() {       // creates empty list with nothing inside except null, doesnt seem to be needed
-        head = null;
+        public treeNode (int x){
+            value = x;
+            lowerChild = null;
+            upperChild = null;
+        }
+
+        public int getValue(){
+            return value;
+        }
+    }
+    // end of treeNode setup
+
+
+    // setting up actual binary tree
+    private treeNode root;      // list is going to use a head and utilize "next" of nodes
+
+    public Tree() {       // constructor
+        root = null;
     }
 
-//    public void add(String x) {
-//        size++;
-//        Node newNode = new Node(x);
-//        if (head == null) {          // case 1, if no words are in the list
-//            head = newNode;          // sets newNode as the head, in which a null is "next" because of default Node constructor
-//        } else {
-//            Node cur = head.getNext();     // keeps track of nodes in the list, set to one ahead
-//            Node prev = head;              // set to one behind
-//            if (newNode.getValue().compareTo(head.getValue()) <= 0) {    // covers if new node is the smallest
-//                newNode.setNext(head);      // "next" of new node becomes the head
-//                head = newNode;             // moves new node to the front, with the ending being null
-//            }
-//            else {
-//                while (cur != null && (newNode.getValue().compareTo(cur.getValue()) >= 0)) { // checks until end of list right before cur becomes null
-//                    prev = cur;
-//                    cur = cur.getNext();
-//                }
-//                newNode.setNext(cur); // sets the "next" of new node
-//                prev.setNext(newNode); // sets the previous nodes "next" as the new node
-//            }
-//        }
-//    }
+    // setting up tree functions
+    public int getRootValue(){
+        return root.getValue();
+    }
+
+    public void add(int x){ // adding x to the bst
+        if (root == null){
+            root = new treeNode(x);
+            System.out.println("New Root: " + x);
+        }
+        else{
+            System.out.println("Adding: " + x);
+            add(root, x);
+        }
+    }
+
+    private void add(treeNode parent, int x){   // recursively adding to subtrees
+        if (x == parent.getValue())
+            System.out.printf("Duplicate value %d will not be added\n", x);
+        else
+            if (x < parent.getValue()){
+                if (parent.lowerChild == null)
+                    parent.lowerChild = new treeNode(x);
+                else
+                    add(parent.lowerChild, x);
+            } else {
+                if (parent.upperChild == null)
+                    parent.upperChild = new treeNode(x);
+                else
+                    add(parent.upperChild, x);
+            }
+    }
+
+    public int size(){      // this function will tell us the size of the entire tree, no parameters
+        if (root == null)   // technically this function is not recursive because it called the private function twice
+            return 0;
+        else
+            return 1 + size(root.lowerChild) + size(root.upperChild);  // this function will tell us the size of each subtree, with parameter for nodes
+    }
+
+    private int size(treeNode parent){  // size of the subtree
+        if (parent == null)  // this function is actually recursive
+            return 0;
+        else
+            return 1 + size(parent.lowerChild) + size(parent.upperChild);
+    }
+
+    public int sum(){   // sum of tree, this will be used to calculate average alongside size
+        if (root == null)
+            return 0;
+        else
+            return root.getValue() + sum(root.lowerChild) + sum(root.upperChild);
+    }
+
+    private int sum(treeNode parent){   // sum of subtrees
+        if (parent == null)
+            return 0;
+        else
+            return parent.getValue() + sum(parent.lowerChild) + sum(parent.upperChild);
+    }
+
+    public void display(){ // in-order: left child, parent, right child
+        if (root == null)
+            System.out.println("Tree is empty");
+        else
+            display(root);
+    }
+
+    private void display(treeNode parent){
+        if (parent == null)
+            return;
+        else {
+            display(parent.lowerChild);     // goes through the smallest to largest
+            System.out.print(parent.getValue() + ", ");
+            display(parent.upperChild);
+        }
+    }
+
+    public int height(){
+        if (root == null)
+            return 0;
+        else
+            return 1 + Math.max(height(root.lowerChild), height(root.upperChild));  // adds the root and the max of the two subtree heights
+    }
+
+    private int height(treeNode parent){
+        if (parent == null)
+            return 0;       // stops if tree gets to the end
+        else
+            return 1 + Math.max(height(parent.lowerChild), height(parent.upperChild));  // traverses through subtrees and finds height, chooses max between the lower and upper childs
+    }
+
+    public int min(){   // big tree
+        if (root == null)
+            return 0;
+        else
+            return min(root);
+    }
+
+    private int min(treeNode parent){   // traverses to the very bottom left which is smallest value in subtrees
+        int min = parent.getValue();
+        if (parent.lowerChild == null)  // stops when it gets to the end i.e. when there is no longer any lower child
+            return min;
+        else
+            return min(parent.lowerChild);  // keeps going if the next parent has a lower child
+    }
+
+    public int max(){
+        if (root == null)
+            return 0;
+        else
+            return max(root);
+    }
+
+    private int max(treeNode parent){   // traverses to the very bottom right which is highest value in subtrees
+        int max = parent.getValue();
+        if (parent.upperChild == null)  // stops when it gets to the end i.e. when there is no longer any upper child
+            return max;
+        else
+            return max(parent.upperChild);  // keeps going if the next parent has a upper child
+    }
 }
--- src/programs/Trees/binarySearchTree.java
+++ src/programs/Trees/binarySearchTree.java
@@ -5,15 +5,14 @@
 
 package programs.Trees;
 
-import programs.LinkedList.Node;
-
+import java.util.Random;
 import java.util.Scanner;
 
 public class binarySearchTree {
     public static void main(String[]arg){
         Scanner in = new Scanner(System.in);
         boolean loop = true;
-        Tree testTree = new Tree();
+        Tree myTree = new Tree();
         while (loop){
             System.out.println("1) Create empty BST");
             System.out.println("2) Add a number to the BST");
@@ -23,25 +22,34 @@ public class binarySearchTree {
             int option = in.nextInt();
             switch (option){
                 case 1:
-                    for (int i = 0; i < 20; i++){
-                    }
+                    myTree = new Tree();
                     break;
                 case 2:
-                    System.out.println();
+                    System.out.println("What number would you like to add?");
+                    int user_num = in.nextInt();
+                    myTree.add(user_num);
                     break;
                 case 3:
-                    System.out.println();
+                    System.out.println("Adding 20 random numbers...");
+                    for (int i = 0; i < 20; i++) {
+                        Random rand = new Random();
+                        int rand_num = rand.nextInt(1000);
+                        myTree.add(rand_num);
+                    }
                     break;
                 case 4:
+                    myTree.display();
                     System.out.println();
                     break;
                 case 5:
-                    System.out.println("1) Root");
-                    System.out.println("2) Tree height");
-                    System.out.println("3) Number of items in the tree");
-                    System.out.println("4) Minimum value");
-                    System.out.println("5) Maximum value");
-                    System.out.println("6) Average value");
+                    System.out.println();
+                    System.out.println("Root: " + myTree.getRootValue());
+                    System.out.println("Tree height: " + myTree.height());
+                    System.out.println("Number of items: " + myTree.size());
+                    System.out.println("Minimum value: " + myTree.min());
+                    System.out.println("Maximum value: " + myTree.max());
+                    System.out.println("Average value: " + myTree.sum() / myTree.size());
+                    System.out.println();
                     break;
                 default:
                     loop = false;
